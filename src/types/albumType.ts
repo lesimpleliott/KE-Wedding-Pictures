ebdc5f66fed2510ac9f70b57baac33d1ab601265
lesimpleliott <131270758+lesimpleliott@users.noreply.github.com>
2024-06-14 17:19:29 +0000

    Build: (f629562) GalleryPhotos // add type

--- src/types/albumType.ts
+++
@@ -1,8 +0,0 @@
-export type AlbumType = {
-  id: string;
-  title: string;
-  downloadLink: string;
-  path: string;
-  cover: string;
-  images: Array<object>;
-};
--- src/utils/ProtectedRoutes.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const ProtectedRoutes = () => {
-  const password = true;
-  return password ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default ProtectedRoutes;
