80d85545cef9f25290ff153aafd835c268451dfb
Eliott Lesimple <lesimpleliott@gmail.com>
2024-06-15 19:49:51 +0200

    HeroBanner // Improve Style

--- src/layouts/HeroBanner.tsx
+++ src/layouts/HeroBanner.tsx
@@ -31,7 +31,7 @@ const HeroBannerStyled = styled.section`
   .banner {
     width: 100%;
     padding: 0.5rem;
-    background-color: rgba(78, 78, 78, 0.3);
+    background-color: rgba(0, 0, 0, 0.2);
     position: absolute;
     top: 35%;
     left: 50%;
@@ -45,27 +45,6 @@ const HeroBannerStyled = styled.section`
       width: 20vw;
       min-width: 250px;
       max-height: 300px;
-    }
-
-    h1 {
-      font-size: 3rem;
-      color: #fff;
-
-      &::after {
-        content: "";
-        display: block;
-        width: 100px;
-        height: 2px;
-        background-color: #fff;
-        margin: 0 auto;
-      }
-    }
-
-    h2 {
-      margin-top: 8px;
-      font-size: 2rem;
-      color: #fff;
-      font-weight: 400;
     }
   }
 `;
