eaeebb176b8f3f059dd228e18268b7c5ef801836
Eliott Lesimple <lesimpleliott@gmail.com>
2024-07-12 12:23:35 +0200

    Debug // Issue #2

--- src/layouts/MasonryLayout.tsx
+++ src/layouts/MasonryLayout.tsx
@@ -1,6 +1,4 @@
-// MasonryLayout.tsx
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import MasonryRow from "../components/masonry/MasonryRow";
 import useWindowSize from "../hooks/useWindowSize";
@@ -12,39 +10,46 @@ type MasonryLayoutProps = {
 
 const MasonryLayout: React.FC<MasonryLayoutProps> = ({ images }) => {
   const [windowWidth] = useWindowSize();
+  const [rows, setRows] = useState<
+    Array<{ images: ImageType[]; height: number }>
+  >([]);
 
-  const createRows = (images: ImageType[], containerWidth: number) => {
-    const rows: Array<{ images: ImageType[]; height: number }> = [];
-    let currentRow: ImageType[] = [];
-    let totalAspectRatio = 0;
+  useEffect(() => {
+    const createRows = (images: ImageType[], containerWidth: number) => {
+      const rows: Array<{ images: ImageType[]; height: number }> = [];
+      let currentRow: ImageType[] = [];
+      let totalAspectRatio = 0;
 
-    images.forEach((image) => {
-      const aspectRatio = image.size.width / image.size.height;
+      images.forEach((image) => {
+        const aspectRatio = image.size.width / image.size.height;
 
-      if (
-        (totalAspectRatio + aspectRatio) * 280 >
-        containerWidth - currentRow.length * 10
-      ) {
+        if (
+          (totalAspectRatio + aspectRatio) * 280 >
+          containerWidth - currentRow.length * 10
+        ) {
+          if (currentRow.length > 0) {
+            const rowHeight = containerWidth / totalAspectRatio;
+            rows.push({ images: currentRow, height: rowHeight });
+          }
+          currentRow = [];
+          totalAspectRatio = 0;
+        }
+
+        currentRow.push(image);
+        totalAspectRatio += aspectRatio;
+      });
+
+      if (currentRow.length > 0) {
         const rowHeight = containerWidth / totalAspectRatio;
         rows.push({ images: currentRow, height: rowHeight });
-        currentRow = [];
-        totalAspectRatio = 0;
       }
 
-      currentRow.push(image);
-      totalAspectRatio += aspectRatio;
-    });
+      return rows;
+    };
 
-    if (currentRow.length > 0) {
-      // Si la dernière ligne n'est pas entièrement remplie, utiliser une hauteur standard de 350px
-      const rowHeight =
-        currentRow.length === 1 ? 350 : containerWidth / totalAspectRatio;
-      rows.push({ images: currentRow, height: rowHeight });
-    }
-
-    return rows;
-  };
-  const rows = createRows(images, windowWidth);
+    const newRows = createRows(images, windowWidth);
+    setRows(newRows);
+  }, [images, windowWidth]);
 
   return (
     <GalleryContainerStyled className="masonryContainer">
